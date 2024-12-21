4621b3b7558d3162029d2b4995fa11d5515b3238
WO1N123 <dan.tk96@mail.ru>
2024-12-21 15:00:18 +0300

    Delete Calculator directory

--- Calculator/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/WO1N123/rpn
-
-go 1.23.1
--- Calculator/pkg/calculation/errors.go
+++
@@ -1,8 +0,0 @@
-package calculation
-
-import "errors"
-
-var (
-	ErrExpressionIsNotValid = errors.New("expression is not valid")//422
-	ErrInternalServerError  = errors.New("internal server error")//500
-)
